fe69d84c2c22bd5dc81fc0e83e7f9b318a4f3129
yq-movis <maxw.mmo@gmail.com>
2025-03-04 17:23:49 -0500

    svg

--- scripts/script-list.js
+++ scripts/script-list.js
@@ -14,3 +14,26 @@ btnList.forEach(btnClicked => {
         showContent.innerHTML = btnClicked.getAttribute("data-content");
     })
 })
+
+
+// Wait for DOM to be loaded
+document.addEventListener("DOMContentLoaded", function() {
+        
+    //select all svg elements with an ID
+    const svgElements = document.querySelectorAll("svg [id]");
+    
+    //add event listeners to each SVG element
+    svgElements.forEach(element => {
+        element.addEventListener("mouseover", function() {
+         //change the colour to highlight the element
+            element.style.fill = "yellow"; 
+        });
+
+        element.addEventListener("mouseout", function() {
+        //revert the fill colour to original state
+            element.style.fill ="";
+        });
+
+    });
+
+}); 
