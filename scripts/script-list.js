79bf5a44cf1c5aab94e3c1b168102efd67797d2c
Max Mo <maxw.mmo@gmail.com>
2025-03-25 12:47:34 -0400

    log 7

--- scripts/script-list.js
+++ scripts/script-list.js
@@ -3,7 +3,6 @@ const btnList = document.querySelectorAll('.entry');
 
 btnList.forEach(btnClicked => {
     btnClicked.addEventListener('click', () => {
-
         
         // same button stays a specified colour until another one is clicked
         document.querySelector('.entry-clicked')?.classList.remove('entry-clicked');
@@ -12,8 +11,11 @@ btnList.forEach(btnClicked => {
         // shows innerHTML, data-content, of class=entry that has the appearance and function of a button
         const showContent = document.getElementById("reflection-content");
         showContent.innerHTML = btnClicked.getAttribute("data-content");
+
     })
 })
+
+
 
 
 // Wait for DOM to be loaded
@@ -26,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
     svgElements.forEach(element => {
         element.addEventListener("mouseover", function() {
          //change the colour to highlight the element
-            element.style.fill = "yellow"; 
+            element.style.fill = "gold"; 
         });
 
         element.addEventListener("mouseout", function() {
@@ -35,5 +37,28 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
     });
+
+
+    var svgacc = document.querySelector(".svgAccordion-btn")
+    //var i; ----- i stands for index
+
+    //for (i = 0; i < acc.length; i++ {  ----- creates loop: each accordion item has an indexed number
+    //  svgacc[i].addEventListener("click", function () { ----- makes each i loops through the same function
+
+    svgacc.addEventListener("click", function() {
+
+        this.classList.toggle("active");
+
+        var svgdd = document.querySelector(".svgDropdown")
+        //checks if dropdown is shown (block) or not (none) and toggles the opposite
+        console.log(svgdd.style.maxHeight)
+        if (svgdd.style.maxHeight) {
+        svgdd.style.maxHeight = null;
+        } 
+        else {
+        svgdd.style.maxHeight = svgdd.scrollHeight + "px";
+        }
+
+     })
 
 }); 
